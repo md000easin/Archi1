f75740112d5a5d6c061cf52e88fe251d51f6fe63
md000easin <md000easin@gmail.com>
2023-08-31 23:52:51 +0600

    first commit

---
+++ resources/js/main.js
@@ -0,0 +1,13 @@
+// NAVIGATION
+
+function openMenu(){
+    document.getElementById("menu-container").style.width = "100%";
+}
+
+function closeMenu(){
+    document.getElementById("menu-container").style.width = "0";
+}
+
+// MIXIITUP
+
+var mixer = mixitup('.work-grid');
